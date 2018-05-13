f1f6c44bf78bc15a631610ad4fb1f78e86a9b8b6
Chris Johnson <chrisjohnsonmail@gmail.com>
2018-05-13 18:49:25 -0500

    Initial add of Lesson 2 stub

---
+++ Sunfounder/Lesson 2/StartupTask.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+using Windows.Devices.Gpio;
+using System.Threading.Tasks;
+using Windows.System.Threading;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace Lesson2
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        BackgroundTaskDeferral deferral;
+        private GpioPinValue value = GpioPinValue.High;
+        private const int LED_PIN = 5;
+        private GpioPin pin;
+        private ThreadPoolTimer timer;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            deferral = taskInstance.GetDeferral();
+            InitGPIO();
+            timer = ThreadPoolTimer.CreatePeriodicTimer(Timer_Tick, TimeSpan.FromMilliseconds(500));
+            
+        }
+        private void InitGPIO()
+        {
+            pin = GpioController.GetDefault().OpenPin(LED_PIN);
+            pin.Write(GpioPinValue.High);
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+        }
+
+        private void Timer_Tick(ThreadPoolTimer timer)
+        {
+            value = (value == GpioPinValue.High) ? GpioPinValue.Low : GpioPinValue.High;
+            pin.Write(value);
+        }
+    }
+}
