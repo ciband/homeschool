2f4433d8bf133c2a4ea21e61b7c6851b615d9556
Chris Johnson <chrisjohnsonmail@gmail.com>
2018-05-13 19:35:33 -0500

    Update lesson 2.  Used samples from:
    https://github.com/Microsoft/Windows-iotcore-samples/tree/develop/Samples/PushButton/CS

--- Sunfounder/Lesson 2/StartupTask.cs
+++ Sunfounder/Lesson 2/StartupTask.cs
@@ -15,29 +15,53 @@ namespace Lesson2
     public sealed class StartupTask : IBackgroundTask
     {
         BackgroundTaskDeferral deferral;
-        private GpioPinValue value = GpioPinValue.High;
+        private GpioPinValue ledPinValue = GpioPinValue.High;
         private const int LED_PIN = 5;
-        private GpioPin pin;
-        private ThreadPoolTimer timer;
+        private const int BUTTON_PIN = 6;
+        private GpioPin ledPin;
+        private GpioPin buttonPin;
 
         public void Run(IBackgroundTaskInstance taskInstance)
         {
             deferral = taskInstance.GetDeferral();
             InitGPIO();
-            timer = ThreadPoolTimer.CreatePeriodicTimer(Timer_Tick, TimeSpan.FromMilliseconds(500));
-            
         }
         private void InitGPIO()
         {
-            pin = GpioController.GetDefault().OpenPin(LED_PIN);
-            pin.Write(GpioPinValue.High);
-            pin.SetDriveMode(GpioPinDriveMode.Output);
+            var controller = GpioController.GetDefault();
+
+            ledPin = controller.OpenPin(LED_PIN);
+            buttonPin = controller.OpenPin(BUTTON_PIN);
+
+            ledPin.Write(GpioPinValue.High);
+            ledPin.SetDriveMode(GpioPinDriveMode.Output);
+
+            // Check if input pull-up resistors are supported
+            if (buttonPin.IsDriveModeSupported(GpioPinDriveMode.InputPullUp))
+            {
+                buttonPin.SetDriveMode(GpioPinDriveMode.InputPullUp);
+            }
+            else
+            {
+                buttonPin.SetDriveMode(GpioPinDriveMode.Input);
+            }
+
+            // Set a debounce timeout to filter out switch bounce noise from a button press
+            buttonPin.DebounceTimeout = TimeSpan.FromMilliseconds(50);
+
+            // Register for the ValueChanged event so our buttonPin_ValueChanged 
+            // function is called when the button is pressed
+            buttonPin.ValueChanged += buttonPin_ValueChanged;
         }
 
-        private void Timer_Tick(ThreadPoolTimer timer)
+        private void buttonPin_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs e)
         {
-            value = (value == GpioPinValue.High) ? GpioPinValue.Low : GpioPinValue.High;
-            pin.Write(value);
+            // toggle the state of the LED every time the button is pressed
+            if (e.Edge == GpioPinEdge.FallingEdge)
+            {
+                ledPinValue = (ledPinValue == GpioPinValue.Low) ? GpioPinValue.High : GpioPinValue.Low;
+                ledPin.Write(ledPinValue);
+            }
         }
-    }
+        }
 }
