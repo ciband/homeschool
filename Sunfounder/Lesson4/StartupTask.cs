8c84b44894f9b1f36fbb4a722de389ca4be9344b
Chris Johnson <chrisjohnsonmail@gmail.com>
2018-07-06 19:48:10 -0500

    Updates to test hardware PWM using Microsoft.IoT.Lightning.  No good so far.

--- Sunfounder/Lesson4/StartupTask.cs
+++ Sunfounder/Lesson4/StartupTask.cs
@@ -7,6 +7,7 @@ using Windows.ApplicationModel.Background;
 using Windows.Devices.Gpio;
 using Windows.Devices.Pwm;
 using System.Threading.Tasks;
+using Microsoft.IoT.Lightning.Providers;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
@@ -19,8 +20,11 @@ namespace Lesson4
         public async void Run(IBackgroundTaskInstance taskInstance)
         {
             deferral = taskInstance.GetDeferral();
-            var controller = await PwmController.GetDefaultAsync();
+            var controllers = await PwmController.GetControllersAsync(LightningPwmProvider.GetPwmProvider());
+            var controller = controllers[1]; // use the on-device controller
+            controller.SetDesiredFrequency(50); // try to match 50Hz
             var pin = controller.OpenPin(18);
+            pin.SetActiveDutyCyclePercentage(0.0);
             pin.Start();
             for (; ; )
             {
