9feb494c42222109621fb72b3b4cc00cf28a9944
Chris <chrisjohnsonmail@gmail.com>
2018-12-24 15:04:36 -0600

    initial working lesson 4

--- Sunfounder/Lesson4/StartupTask.cs
+++ Sunfounder/Lesson4/StartupTask.cs
@@ -7,7 +7,8 @@ using Windows.ApplicationModel.Background;
 using Windows.Devices.Gpio;
 using Windows.Devices.Pwm;
 using System.Threading.Tasks;
-using Microsoft.IoT.Lightning.Providers;
+using Microsoft.IoT.DeviceCore.Pwm;
+using Microsoft.IoT.Devices.Pwm;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
@@ -20,23 +21,29 @@ namespace Lesson4
         public async void Run(IBackgroundTaskInstance taskInstance)
         {
             deferral = taskInstance.GetDeferral();
-            var controllers = await PwmController.GetControllersAsync(LightningPwmProvider.GetPwmProvider());
-            var controller = controllers[1]; // use the on-device controller
-            controller.SetDesiredFrequency(50); // try to match 50Hz
-            var pin = controller.OpenPin(18);
-            pin.SetActiveDutyCyclePercentage(0.0);
+            var pwmManager = new PwmProviderManager();
+            pwmManager.Providers.Add(new SoftPwm());
+            var pwmControllers = await pwmManager.GetControllersAsync();
+
+            //use the first available PWM controller an set refresh rate (Hz)
+           var pwmController = pwmControllers[0];
+            pwmController.SetDesiredFrequency(50);
+
+            var pin = pwmController.OpenPin(18);
             pin.Start();
+            //slDutyCycle.Value = 100;
+
             for (; ; )
             {
                 for (var i = 0; i <= 100; ++i)
                 {
-                    pin.SetActiveDutyCyclePercentage(i);
+                    pin.SetActiveDutyCyclePercentage(i / 100.0);
                     await Task.Delay(TimeSpan.FromMilliseconds(20));
                 }
                 await Task.Delay(TimeSpan.FromSeconds(1));
                 for (var i = 100; i >= 0; --i)
                 {
-                    pin.SetActiveDutyCyclePercentage(i);
+                    pin.SetActiveDutyCyclePercentage(i / 100.0);
                     await Task.Delay(TimeSpan.FromMilliseconds(20));
                 }
             }
